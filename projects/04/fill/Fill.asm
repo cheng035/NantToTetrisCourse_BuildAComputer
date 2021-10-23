aaa10c6e6cf984c4968d709fcad9090d468e4c9c
Cheng Haohan <haohan.cheng@shopee.com>
2021-10-23 17:54:16 +0800

    :deploy: implement dark the screen operation

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,81 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//set the number of register we need to light
+@24576
+D=A
+
+@n
+M=D
+
+
+
+(LOOP)
+//reset scr 
+	@SCREEN
+	D=A
+
+	@scr
+	M=D
+	
+	@KBD //get the keyboard
+	D=M
+	
+	@LOOP  //if the keyboard is not pressed, go back
+	D;JEQ 
+	
+	//else dark the screen
+	(LOOP2)  // LOOP FOR LIGHT
+		@scr //get the address of screen
+		A=M
+		M=-1
+		
+		
+		
+		@scr
+		M=M+1
+		
+		@n // get total value
+		D=M
+		
+		@scr
+		D=D-M  //get the value of n-i
+		
+	@LOOP2
+	D;JGT
+	
+	//turn it back to white
+	
+	@SCREEN
+	D=A
+
+	@scr
+	M=D
+		(LOOP3)  // LOOP FOR LIGHT
+		@scr //get the address of screen
+		A=M
+		M=0
+		
+		
+		
+		@scr
+		M=M+1
+		
+		@n // get total value
+		D=M
+		
+		@scr
+		D=D-M  //get the value of n-i
+		
+	@LOOP3
+	D;JGT
+	 
+	
+	
+	
+	
+
+@LOOP
+0;JMP
+
